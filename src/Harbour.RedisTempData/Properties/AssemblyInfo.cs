2f66a826cf74ac9d0f4846b112f4fdd28246f4bb
Adrian Phinney <adrian.phinney@gmail.com>
2016-02-02 14:47:26 -0500

    Bump version to 3.0.0.

--- src/Harbour.RedisTempData/Properties/AssemblyInfo.cs
+++ src/Harbour.RedisTempData/Properties/AssemblyInfo.cs
@@ -6,14 +6,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("http://github.com/TheCloudlessSky/Harbour.RedisTempData")]
 [assembly: AssemblyProduct("Harbour.RedisTempData")]
-[assembly: AssemblyCopyright("Copyright © Harbour Inc. 2015")]
+[assembly: AssemblyCopyright("Copyright © Harbour Inc. 2016")]
 [assembly: AssemblyDescription("An ASP.NET MVC ITempDataProvider for Redis.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
 [assembly: Guid("f496a810-dffb-4c19-a12d-f0730dce3138")]
-[assembly: AssemblyVersion("2.2.1.0")]
-[assembly: AssemblyFileVersion("2.2.1.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 
 [assembly: InternalsVisibleTo("Harbour.RedisTempData.Test")]

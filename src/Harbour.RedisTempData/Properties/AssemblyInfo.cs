287553816ee85fd7a42ea46ddcbd2c49980168d1
Adrian Phinney <adrian.phinney@gmail.com>
2023-03-07 11:12:16 -0500

    Prepare 4.0.0 release.

--- src/Harbour.RedisTempData/Properties/AssemblyInfo.cs
+++ src/Harbour.RedisTempData/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Harbour.RedisTempDataProvider")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("http://github.com/TheCloudlessSky/Harbour.RedisTempData")]
+[assembly: AssemblyCompany("https://github.com/TheCloudlessSky/Harbour.RedisTempData")]
 [assembly: AssemblyProduct("Harbour.RedisTempData")]
-[assembly: AssemblyCopyright("Copyright © Harbour Inc. 2016")]
+[assembly: AssemblyCopyright("Copyright © Harbour Inc. 2023")]
 [assembly: AssemblyDescription("An ASP.NET MVC ITempDataProvider for Redis.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
 [assembly: Guid("f496a810-dffb-4c19-a12d-f0730dce3138")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 
 [assembly: InternalsVisibleTo("Harbour.RedisTempData.Test")]

7f1ed0e67b68f9788a59c0a21d911a439449ca70
Brandon Yanuziello <brandon.yanuziello@procedureflow.com>
2023-03-09 17:02:54 -0500

    - Updated nuget cli to version 4.1.0 - made changes required for nuget upgrade - Added publishing section to ReadMe

--- psake.cmd
+++ psake.cmd
@@ -1,1 +1,3 @@
+.\.nuget\nuget.exe install psake -Version 4.9.0 -OutputDirectory packages && ^
+.\.nuget\nuget.exe install xunit.runners -Version 1.9.2 -OutputDirectory packages && ^
 .\packages\psake.4.9.0\tools\psake\psake.cmd -buildFile .\build\default.ps1 %*
